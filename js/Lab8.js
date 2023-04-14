22d3247e556981c1362aa15c49436c2b418fca7b
Sokim Nam <namsokim9@gmail.com>
2023-04-13 21:23:35 -0500

    lab 7

---
+++ js/Lab8.js
@@ -0,0 +1,102 @@
+// // Question 1: Object literal
+// let student = {
+//     firstName : "",
+//     lastName : "",
+//     grades : [],
+//     inputNewGrade : function(newGrade){
+//         for(let i=0; i<newGrade.length; i++){
+//             this.grades.push(newGrade[i]);
+//         }
+//         console.log(this.grades)
+//     },
+//     computeAverageGrade : function(){
+//         var sum = 0;
+//         for(let x in this.grades){
+//             sum += x;
+//         }
+//         return sum/this.grades.length;
+//     }
+// };
+
+// let grade = [67,54,45,55];
+// console.log(student.inputNewGrade(grade));
+// console.log(student.grades);
+// console.log(student.computeAverageGrade());
+
+// let student1 = Object.create(student);
+// student1.name = "Sokim Nam";
+// student1.age = 24;
+// student1.grade = [34,45,35,55];
+
+// let student2 = Object.create(student);
+// student2.name = "Peter Nam";
+// student2.age = 20;
+// student2.grade = [44,32,65,34];
+
+// let studentArray = [
+//     { name : student1.name, age:student1.age, grade: student1.grade},
+//     { name : student2.name, age:student2.age, grade: student2.grade}
+// ];
+
+function calculateGrade(studentList){
+    for(let i=0; i<studentList.length;i++){
+        var total = 0;
+        var count= 0;
+        var average = 0;
+        for(let j=0; j<studentList[i].grade.length; j++){
+            total += studentList[i].grade[j];
+            count++;
+        }
+        average = total/count;
+        console.log(`The result is: ` + average);
+    }
+    return "------------------";
+    
+}
+//  console.log(calculateGrade(studentArray));
+
+//Question 2: Constructor Function
+function Student(firstName, lastName, grades){
+    this.fName = firstName;
+    this.lName = lastName;
+    this.grades = grades;
+}
+Student.prototype.inputNewGrade = function(newGrade){
+    for(let i=0; i<newGrade.length; i++){
+        console.log(newGrade[i]);
+        return this.grades.push(newGrade[i]);
+     }
+};
+Student.prototype.computeAverageGrade= function(){
+    var sum = 0;
+    for(let x in Student.grades){
+        sum += x;
+    }
+    return sum/Student.grades.length;
+};
+Student.prototype.sort = function() {
+    const n = this.length;
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (this[j] > this[j + 1]) {
+          const temp = this[j];
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+    return this;
+  };
+
+const student1 = new Student("Sokim", "Nam", [67,54,45,55]);
+const student2 = new Student("Kinin", "Roza", [55,34,48,100]);
+const student3 = new Student("Ratanak", "Chhay", [45,67,99,12]);
+
+let studentArray = [
+        { firstName : student1.fName, lastName:student1.lName, grade: student1.grades},
+        { firstName : student2.fName, lastName:student2.lName, grade: student2.grades},
+        { firstName : student3.fName, lastName:student3.lName, grade: student3.grades}
+];
+console.log(calculateGrade(studentArray));
+// studentArray.sort((a, b) => a.firstName.localeCompare(b.lastName));
+// console.log(calculateGrade(studentArray).sort())
---
+++ js/lab7.js
@@ -0,0 +1,29 @@
+// function askPassword(ok, fail){
+//     let password = prompt("Password?", '');
+//     if(password == "rockstar") ok();
+//     else fail();
+// }
+// let user = {
+//     name : 'John',
+
+//     loginOk: function(){
+//         alert(`${this.name} logged in`);
+//     },
+//     loginFail: function(){
+//         alert(`${this.name} failed to log in`);
+//     },
+// };
+
+// askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+
+//Question 2
+let group = {
+    title : "Our Group",
+    students : ["John", "Pete", "Alice"],
+    showList:function(){
+        this.students.forEach(function(student){
+            console.log(this.title + ": " + student);
+        }.bind(this));
+    }
+};
+group.showList();
