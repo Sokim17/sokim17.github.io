4af30b3c58088b3fd7b3e5ba7a5912e82ecce0f0
Sokim Nam <namsokim9@gmail.com>
2023-04-13 21:26:49 -0500

    lab 7

--- js/lab7.js
+++ js/lab7.js
@@ -1,20 +1,20 @@
-// function askPassword(ok, fail){
-//     let password = prompt("Password?", '');
-//     if(password == "rockstar") ok();
-//     else fail();
-// }
-// let user = {
-//     name : 'John',
+function askPassword(ok, fail){
+    let password = prompt("Password?", '');
+    if(password == "rockstar") ok();
+    else fail();
+}
+let user = {
+    name : 'John',
 
-//     loginOk: function(){
-//         alert(`${this.name} logged in`);
-//     },
-//     loginFail: function(){
-//         alert(`${this.name} failed to log in`);
-//     },
-// };
+    loginOk: function(){
+        alert(`${this.name} logged in`);
+    },
+    loginFail: function(){
+        alert(`${this.name} failed to log in`);
+    },
+};
 
-// askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
 
 //Question 2
 let group = {
